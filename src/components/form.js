a5e428ebad4371d250687ac95d4f86014098ffbd
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2022-09-07 21:31:16 +0200

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -9,37 +9,35 @@ const handleSubmit =  useCallback(async () => {
 });
 return(
     <>
-    <form onSubmit={handleSubmit}>
-                <div>
-                <label> Google Drive link of your ID front:
-                  <input 
-                    type="text" 
-                    value={IDFront}
-                    onChange={(e) => setFront(e.target.value)}
-                  />
-                </label>
-                </div>
-                <div>
-                <label>Google Drive link of your ID Back:
-                  <input 
-                    type="text" 
-                    value={IDBack}
-                    onChange={(e) => setBack(e.target.value)}
-                  />
-                </label>
-                </div>
-
-                <div>
-                <label>Google Drive link of your Photo with the ID:
-                  <input 
-                    type="text" 
-                    value={photoID}
-                    onChange={(e) => setPhoto(e.target.value)}
-                  />
-                </label>
-                </div>
-                  <input type="submit"/>
-              </form>
+    <h1>Please fill this for to verify your profile</h1>
+    <div>
+        <label> Google Drive link of your ID front:
+            <input 
+                type="text" 
+                value={IDFront}
+                onChange={(e) => setFront(e.target.value)}
+            />
+        </label>
+    </div>
+    <div>
+        <label>Google Drive link of your ID Back:
+            <input 
+                type="text" 
+                value={IDBack}
+                onChange={(e) => setBack(e.target.value)}
+            />
+        </label>
+    </div>
+    <div>
+        <label>Google Drive link of your Photo with the ID:
+            <input 
+                type="text" 
+                value={photoID}
+                onChange={(e) => setPhoto(e.target.value)}
+            />
+        </label>
+    </div>
+    <button onClick={handleSubmit}> Submit Form</button>
     </>
 )
 };
