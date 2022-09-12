e24198717c8ebe991bff9b771338ae4cb261812e
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2022-09-12 16:10:36 +0200

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,34 @@
 import React, { useEffect, useCallback, useState } from "react";
 import Direct from "./Direct"
-import addVerification from "./Functions"
+import Loader from "./utils/Loader";
+import {
+    verifyAccount,
+  } from "./utils/functions";
 const Form = ({userID}) => {
 const [ID, setUserID] = useState(userID);
+const [loading, setLoading] = useState(false);
 const [showform, setForm] = useState(true);
+
+const addVerification = async (userId , type) => {
+    try {
+      setLoading(true);
+      await verifyAccount(userId, type);
+      setForm(false)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    };
+  }
 return (
     <>
-    {showform ? (
-        <button onClick={() => addVerification( ID  , 1)}> Submit Form </button>
-    ): ( <Direct ID = {ID}/>)}
+    {!loading ? (
+        <div>
+        {showform ? (
+            <button onClick={() => addVerification( ID  , 1)}> Submit Form </button>
+        ) : ( <Direct ID = {ID}/>)}
+        </div>
+    ): ( <Loader />)}
     </>
 )
 };
