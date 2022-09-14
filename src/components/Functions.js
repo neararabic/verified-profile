237194b11a8745872b007a893d8eec82da8402d6
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2022-09-14 13:25:31 +0200

    restore

--- src/components/Functions.js
+++ src/components/Functions.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import Loader from "./utils/Loader";
+import Loader from "./utils/loader";
 import Direct from "./Direct"
 import {
   createProfile,
