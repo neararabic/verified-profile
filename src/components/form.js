770300de4d4aa38caa585feb881de1d5fc5d1f4b
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2022-09-16 16:09:47 +0200

    update

--- src/components/form.js
+++ src/components/form.js
@@ -1,105 +1,123 @@
 import React, { useEffect, useCallback, useState } from "react";
-import Direct from "./Direct"
+import Direct from "./Direct";
 import Loader from "./utils/loader";
-import storage from "./storageconfig"
-import db from "./storageconfig"
+import { colRef } from "./storageconfig";
+import storage from "./storageconfig";
+import { doc, setDoc } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import {
-    verifyAccount,
-  } from "./utils/functions";
-  
-const Form = ({userID}) => {
-const [ID] = useState(userID);
-const [loading, setLoading] = useState(false);
-const [showform, setForm] = useState(true);
-const [file, setFile] = useState([]);
-const [percent, setPercent] = useState(0);
-const [customerName, setCustomerName] = useState("");
-const [urls, setUrls] = useState([]);
+import { verifyAccount } from "./utils/functions";
 
-const addVerification = async (userId , type) => {
+const Form = ({ userID }) => {
+  const [ID] = useState(userID);
+  const [loading, setLoading] = useState(false);
+  const [showform, setForm] = useState(true);
+  var [urls] = useState([]);
+
+  const addVerification = async (userId, type) => {
     try {
       setLoading(true);
       await verifyAccount(userId, type);
-      setForm(false)
+      setForm(false);
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
-    };
-  }
-  function handleChange(e) {
-    for (let i = 0; i < e.target.files.length; i++) {
-    const Newfile = e.target.files[i];
-    setFile((prevState) => [...prevState, Newfile]) 
-    }   
-}
-const handleUpload = () => {
-  if (file.length !== 3) {
-      alert("Please upload 3 pictures");
-  }
-  else
-  {
-  const promises = [];
-  file.map((image) => {
-  const storageRef = ref(storage, `/files/${image.name}`);
-  const uploadTask = uploadBytesResumable(storageRef, image);
-  promises.push(uploadTask);
-  uploadTask.on(
+    }
+  };
+  function handleFrontID(e) {
+    const Newfile = e.target.files[0];
+    const storageRef = ref(storage, `/files/${Newfile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, Newfile);
+    uploadTask.on(
       "state_changed",
+      (snapshot) => {
+      },
       (err) => console.log(err),
-      async () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+      () => {
+        // download url
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
           console.log(url);
-            setUrls((prevState) => [...prevState, urls]);
-          });
-        }
-      );
+          urls[0] = url;
+        });
+      }
+    );
+  }
+  function handleBackID(e) {
+    const Newfile = e.target.files[0];
+    const storageRef = ref(storage, `/files/${Newfile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, Newfile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+      },
+      (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+          urls[1] = url;
+        });
+      }
+    );
+  }
+  function handlePicWithID(e) {
+    const Newfile = e.target.files[0];
+    const storageRef = ref(storage, `/files/${Newfile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, Newfile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+      },
+      (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+          urls[2] = url;
+        });
+      }
+    );
+  }
+
+  const submit = async () => {
+    console.log(urls);
+    await setDoc(doc(colRef, ID), {
+      FrontID: urls[0],
+      BackID: urls[1],
+      PicWithID: urls[2],
     });
     addVerification( ID  , 1)
-  }
-  }
-  const submit = (e) => {
-    e.preventDefault();
-    db.collection("Profiles").add({
-      name: customerName,
-    });
-  }
+  };
   const style = {
     padding: "10px",
-    fontSize: "24px"
+    fontSize: "24px",
+    borderRightWidth: "10 px",
   };
-return (
+  return (
     <>
-    {!loading ? (
+      {!loading ? (
         <div>
-        {showform ? (
-          <div>
-          <div style={style}> 
-            <p1>To be verified successfully Please upload 3 Pictures</p1>  
-          </div>
-          <div style={style}>1. Picture of the ID front</div>
-          <div style={style}>2. Picture of the ID back</div>
-          <div style={style}>3. Picture of you with the ID</div>
-          <div> 
-          <input  type="file" multiple onChange={handleChange} />          
-          </div>
-          <div> 
-          <button  onClick={handleUpload}> Submit Form </button>
-          </div>
-          <div>       
-          <input
-          type="text"
-          placeholder="Name"
-          value={customerName}
-          onChange={(e) => setCustomerName(e.target.value)}/>
-          <button onClick={submit}>Submit</button>
-          </div>
-          </div>
-        ) : ( <Direct ID = {ID}/>)}
+          {showform ? (
+            <div>
+              <div style={style}>
+                <p1>To be verified successfully Please upload 3 Pictures</p1>
+              </div>
+              <div style={style}>1. Picture of the ID front</div>
+              <input type="file" onChange={handleFrontID} />
+              <div style={style}>2. Picture of the ID back</div>
+              <input type="file" onChange={handleBackID} />
+              <div style={style}>3. Picture of you with the ID</div>
+              <input type="file" onChange={handlePicWithID} />
+              <div>
+                <button onClick={submit}> Submit Form </button>
+              </div>
+            </div>
+          ) : (
+            <Direct ID={ID} />
+          )}
         </div>
-    ): ( <Loader />)}
+      ) : (
+        <Loader />
+      )}
     </>
-)
+  );
 };
-export default Form
+export default Form;
--- src/components/storageconfig.js
+++ src/components/storageconfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
-
- 
+import { getFirestore } from 'firebase/firestore';
+import { collection } from "firebase/firestore";
 // Initialize Firebase
-const app = initializeApp ({
-    apiKey: "AIzaSyBvcOeJbT0Sn6a40Aj2xTfzJRhDt5GnWek",
+const firebaseConfig = {
 
+    apiKey: "AIzaSyBvcOeJbT0Sn6a40Aj2xTfzJRhDt5GnWek",
+  
     authDomain: "verified-profile.firebaseapp.com",
   
     projectId: "verified-profile",
@@ -19,10 +19,17 @@ const app = initializeApp ({
   
     appId: "1:811507154375:web:a99a1d71c063273cc96de2",
   
-    measurementId: "G-MKT1969NCS",
-});
+    measurementId: "G-MKT1969NCS"
+  
+  };
  
+
+const app = initializeApp(firebaseConfig);
+
 // Firebase storage reference
 const storage = getStorage(app);
-const db = getFirestore(app)
-export default {storage,db};
+const db = getFirestore(app);
+export const colRef = collection(db, "Profiles");
+
+
+export default storage;
