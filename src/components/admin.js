97c67d3ed2bd1334864a3ef8ee76a0f802414b93
aliabdallah7 <ali.abdallah43792@gmail.com>
2022-09-19 13:05:38 +0200

    Update

--- src/components/admin.js
+++ src/components/admin.js
@@ -43,17 +43,6 @@ const Admin = () => {
     }
   };
 
-  // const getPending = async () => {
-  //     try {
-  //       setLoading(true);
-  //       setPending(await getPendingUsers())
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
   const refresh = async () => {
     try {
       setLoading(true);
@@ -79,50 +68,6 @@ const Admin = () => {
       setLoading(false);
     }
   };
-
-  //   const getNew = async () => {
-  //     try {
-  //       setLoading(true);
-  //       setNew(await getNewUsers())
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   const getRejected = async () => {
-  //     try {
-  //       setLoading(true);
-  //       setRejected(await getRejectedUsers())
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   const getVerified = async () => {
-  //     try {
-  //       setLoading(true);
-  //       setVerified(await getVerifiedUsers())
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   const getSpam = async () => {
-  //     try {
-  //       setLoading(true);
-  //       setSpam(await getSpamUsers())
-  //     } catch (error) {
-  //       console.log(error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
 
   useEffect(() => {
     getUsers();
@@ -157,6 +102,7 @@ const Admin = () => {
     <>
       {!loading ? (
         <>
+
           <div style={{ background: "#000" }}>
             <div>
               <h1>New</h1>
@@ -238,35 +184,7 @@ const Admin = () => {
               </ol>
             </div>
           </div>
-          {/* <ol>
-             {pendingList.map((pendingList) => (
-              <li>{pendingList}   "Pending"   <button onClick={() => verifyUser(pendingList).then(getPending)}>Verify</button> </li>
-            ))}
-            </ol>
 
-            <ol>
-             {newList.map((newList) => (
-              <li>{newList}   "New"</li>
-            ))}
-            </ol>
-
-            <ol>
-             {rejectedList.map((rejectedList) => (
-              <li>{rejectedList}   "Rejected"</li>
-            ))}
-            </ol>
-
-            <ol>
-             {verifiedList.map((verifiedList) => (
-              <li>{verifiedList}   "Verified"</li>
-            ))}
-            </ol>
-
-            <ol>
-             {spamList.map((spamList) => (
-              <li>{spamList}   "Spam"</li>
-            ))}
-            </ol> */}
         </>
       ) : (
         <Loader />
--- src/components/utils/functions.js
+++ src/components/utils/functions.js
@@ -13,22 +13,6 @@ export  function verificationType(ID) {
   return window.contract.verificationPerUser({ accountID :  ID});
 }
 
-export  function getPendingUsers() {
-  return window.contract.getPendingUsers();
-}
-export  function getNewUsers() {
-  return window.contract.getNewUsers();
-}
-export  function getVerifiedUsers() {
-  return window.contract.getVerifiedUsers();
-}
-export  function getRejectedUsers() {
-  return window.contract.getRejectedUsers();
-}
-export  function getSpamUsers() {
-  return window.contract.getSpamUsers();
-}
-
 export async function verifyAccount(ID,Type){
   await window.contract.verifyAccount({accountID : ID, verification : Type } ,GAS); 
 }
